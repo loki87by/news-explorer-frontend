a86c369a20bca46b282d275bbd89c6c4a6da9610
Alexey Akulich <loki1987by@yandex.ru>
2020-10-16 19:40:20 +0300

    сделать каркас app

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+// **импорты
+import React from 'react';
+import Header from '../Header';
+import Main from '../Main';
+import Footer from '../Footer';
+import './App.css';
+
+// **Функционал
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+};
+
+export default App;

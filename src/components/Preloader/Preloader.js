805a89d8f25511b3c035c6675d8efddfad160fc8
Alexey Akulich <loki1987by@yandex.ru>
2020-10-17 23:23:27 +0300

    запилить макет прелоудера

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+// **импорты
+import React from 'react';
+import './Preloader.css';
+
+// **Функционал
+function Preloader() {
+  return (
+    <link rel="stylesheet" href="./Preloader.css" />
+  )
+};
+
+export default Preloader;

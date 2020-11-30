944cac31f109622990832d7627e2c944f2fcdf25
Alexey Akulich <loki1987by@yandex.ru>
2020-12-01 00:46:32 +0300

    Updates

---
+++ precache-manifest.3ccf2886619556fb694cd8efa3bc5d33.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc4fc01db532062cdbc95eab3990d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec59c94a2972527fbfeb",
+    "url": "/static/css/main.a9df9768.chunk.css"
+  },
+  {
+    "revision": "1049207b38fcb469c29f",
+    "url": "/static/js/2.6349b654.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6349b654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec59c94a2972527fbfeb",
+    "url": "/static/js/main.e2be1c12.chunk.js"
+  },
+  {
+    "revision": "c60840167acfbb45b912",
+    "url": "/static/js/runtime-main.18fc895e.js"
+  },
+  {
+    "revision": "396173f0904df40b101498ef51444d98",
+    "url": "/static/media/82f1206f112335e2ee4d938ba64f02d6-min.396173f0.jpg"
+  },
+  {
+    "revision": "582f6e9e8c112023dfdff87cade2f431",
+    "url": "/static/media/authorAvatar-min.582f6e9e.jpg"
+  },
+  {
+    "revision": "4521f6ae7ea044e9089e044d0cbb6ea7",
+    "url": "/static/media/facebook.4521f6ae.svg"
+  },
+  {
+    "revision": "7ea7bee94b1fb3d94ec1e43100d27017",
+    "url": "/static/media/github.7ea7bee9.svg"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/static/media/inter-regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/static/media/sourcesanspro-regular.49d8d828.woff"
+  },
+  {
+    "revision": "068c1672f84d2c73230f444ff0c2d058",
+    "url": "/static/media/trash-unhover.068c1672.svg"
+  },
+  {
+    "revision": "1b88fb377112e88e468ae9a507229ba5",
+    "url": "/static/media/trash.1b88fb37.svg"
+  }
+]);
